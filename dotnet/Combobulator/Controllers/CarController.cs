0dfb1c307606b663e24fca7cacbd6f8b33e9e3c1
Keith Vong <keith.vong@live.co.uk>
2015-01-29 10:58:40 +0000

    error handling

--- dotnet/Combobulator/Controllers/CarController.cs
+++ dotnet/Combobulator/Controllers/CarController.cs
@@ -19,58 +19,76 @@ namespace Combobulator.Controllers
         private int apiCacheDurationHours = Convert.ToInt32(ConfigurationManager.AppSettings["ApiCacheDurationHours"]);
         private bool apiCacheEnabled = Convert.ToBoolean(ConfigurationManager.AppSettings["ApiCacheEnabled"]);
         private Combobulator.DAL.CombobulatorDataContext dbContext = new Combobulator.DAL.CombobulatorDataContext();
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         // GET api/car
         // Gets all cars
         [System.Web.Mvc.HttpGet]
         public HttpResponseMessage GetCars()
         {
-            IList<Combobulator.DAL.Car> dbCar = dbContext.GetCars().ToList();
-            var query = from c in dbCar select new Car 
-            {
-                Id = c.Id,
-                Model = c.Model,
-                ModelCode = c.ModelCode,
-                Colour = c.Colour,
-                Engine = c.Engine,
-                DisplayName = c.DisplayName,
-                Type = c.Type,
-                CapacityScale = c.CapacityScale,
-                LuggageScale = c.LuggageScale,
-                Options = c.Options,
-                PriceScale = c.PriceScale,
-                Cost = c.Cost,
-                PerformanceScale = c.PerformanceScale,
-                MPH = c.MPH,
-                EconomyScale = c.EconomyScale,
-                MPG = c.MPG,
-                UsageScale = c.UsageScale,
-                Alt1 = c.Alt1,
-                Alt2 = c.Alt2,
-                Alt3 = c.Alt3,
-                TermsConditions = c.TermsConditions
-            };
-
-            IEnumerable<Car> cars = query.ToList();
-
-            if (cars == null)
-            {
-                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
-            }
             var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-
-            StringContent sc = new StringContent(serializer.Serialize(cars).ToString());
-            sc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
             HttpResponseMessage response = new HttpResponseMessage();
-            response.Content = sc;
-            if (apiCacheEnabled)
+
+            try
             {
-                response.Headers.CacheControl = new CacheControlHeaderValue();
-                response.Headers.CacheControl.MaxAge = new TimeSpan(apiCacheDurationHours, 0, 0);
-                response.Headers.CacheControl.Public = true;
+                IList<Combobulator.DAL.Car> dbCar = dbContext.GetCars().ToList();
+                var query = from c in dbCar
+                            select new Car
+                                {
+                                    Id = c.Id,
+                                    Model = c.Model,
+                                    ModelCode = c.ModelCode,
+                                    Colour = c.Colour,
+                                    Engine = c.Engine,
+                                    DisplayName = c.DisplayName,
+                                    Type = c.Type,
+                                    CapacityScale = c.CapacityScale,
+                                    LuggageScale = c.LuggageScale,
+                                    Options = c.Options,
+                                    PriceScale = c.PriceScale,
+                                    Cost = c.Cost,
+                                    PerformanceScale = c.PerformanceScale,
+                                    MPH = c.MPH,
+                                    EconomyScale = c.EconomyScale,
+                                    MPG = c.MPG,
+                                    UsageScale = c.UsageScale,
+                                    Alt1 = c.Alt1,
+                                    Alt2 = c.Alt2,
+                                    Alt3 = c.Alt3,
+                                    TermsConditions = c.TermsConditions
+                                };
+
+                IEnumerable<Car> cars = query.ToList();
+
+                if (cars == null)
+                {
+                    throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+                }
+
+                StringContent sc = new StringContent(serializer.Serialize(cars).ToString());
+                sc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response.Content = sc;
+
+                if (apiCacheEnabled)
+                {
+                    response.Headers.CacheControl = new CacheControlHeaderValue();
+                    response.Headers.CacheControl.MaxAge = new TimeSpan(apiCacheDurationHours, 0, 0);
+                    response.Headers.CacheControl.Public = true;
+                }
+                return response;
             }
-            return response;
+            catch (Exception ex)
+            {
+                log.Error("GetCars", ex);
+                ErrorResponse error = new ErrorResponse
+                {
+                    Error = "Failed to get cars"
+                };
+                StringContent sc = new StringContent(serializer.Serialize(error).ToString());
+                sc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response.Content = sc;
+                return response;
+            }
         }
 
         // GET api/car/XN12
@@ -78,50 +96,66 @@ namespace Combobulator.Controllers
         [System.Web.Mvc.HttpGet]
         public HttpResponseMessage GetCar(string id)
         {
-            Combobulator.DAL.Car dbCar = dbContext.GetCar(id).FirstOrDefault();
-            Car car = new Car
-            {
-                Id = dbCar.Id,
-                Model = dbCar.Model,
-                ModelCode = dbCar.ModelCode,
-                Colour = dbCar.Colour,
-                Engine = dbCar.Engine,
-                DisplayName = dbCar.DisplayName,
-                Type = dbCar.Type,
-                CapacityScale = dbCar.CapacityScale,
-                LuggageScale = dbCar.LuggageScale,
-                Options = dbCar.Options,
-                PriceScale = dbCar.PriceScale,
-                Cost = dbCar.Cost,
-                PerformanceScale = dbCar.PerformanceScale,
-                MPH = dbCar.MPH,
-                EconomyScale = dbCar.EconomyScale,
-                MPG = dbCar.MPG,
-                UsageScale = dbCar.UsageScale,
-                Alt1 = dbCar.Alt1,
-                Alt2 = dbCar.Alt2,
-                Alt3 = dbCar.Alt3,
-                TermsConditions = dbCar.TermsConditions
-            };
-
-            if (car == null)
-            {
-                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
-            }
             var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-
-            StringContent sc = new StringContent(serializer.Serialize(car).ToString());
-            sc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
             HttpResponseMessage response = new HttpResponseMessage();
-            response.Content = sc;
-            if (apiCacheEnabled)
+
+            try
             {
-                response.Headers.CacheControl = new CacheControlHeaderValue();
-                response.Headers.CacheControl.MaxAge = new TimeSpan(apiCacheDurationHours, 0, 0);
-                response.Headers.CacheControl.Public = true;
+                Combobulator.DAL.Car dbCar = dbContext.GetCar(id).FirstOrDefault();
+                Car car = new Car
+                {
+                    Id = dbCar.Id,
+                    Model = dbCar.Model,
+                    ModelCode = dbCar.ModelCode,
+                    Colour = dbCar.Colour,
+                    Engine = dbCar.Engine,
+                    DisplayName = dbCar.DisplayName,
+                    Type = dbCar.Type,
+                    CapacityScale = dbCar.CapacityScale,
+                    LuggageScale = dbCar.LuggageScale,
+                    Options = dbCar.Options,
+                    PriceScale = dbCar.PriceScale,
+                    Cost = dbCar.Cost,
+                    PerformanceScale = dbCar.PerformanceScale,
+                    MPH = dbCar.MPH,
+                    EconomyScale = dbCar.EconomyScale,
+                    MPG = dbCar.MPG,
+                    UsageScale = dbCar.UsageScale,
+                    Alt1 = dbCar.Alt1,
+                    Alt2 = dbCar.Alt2,
+                    Alt3 = dbCar.Alt3,
+                    TermsConditions = dbCar.TermsConditions
+                };
+
+                if (car == null)
+                {
+                    throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+                }
+                
+                StringContent sc = new StringContent(serializer.Serialize(car).ToString());
+                sc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response.Content = sc;
+
+                if (apiCacheEnabled)
+                {
+                    response.Headers.CacheControl = new CacheControlHeaderValue();
+                    response.Headers.CacheControl.MaxAge = new TimeSpan(apiCacheDurationHours, 0, 0);
+                    response.Headers.CacheControl.Public = true;
+                }
+                return response;
             }
-            return response;
+            catch (Exception ex)
+            {
+                log.Error("GetCar", ex);
+                ErrorResponse error = new ErrorResponse
+                {
+                    Error = "Failed to get car"
+                };
+                StringContent sc = new StringContent(serializer.Serialize(error).ToString());
+                sc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response.Content = sc;
+                return response;
+            }
         }
     }
 }
--- dotnet/Combobulator/Controllers/CustomerController.cs
+++ dotnet/Combobulator/Controllers/CustomerController.cs
@@ -15,75 +15,6 @@ namespace Combobulator.Controllers
         private Combobulator.DAL.CombobulatorDataContext dbContext = new Combobulator.DAL.CombobulatorDataContext();
 
         [HttpPost]
-        public ActionResult NewCustomerSubmit(FormCollection collection)
-        {
-            try
-            {
-                Customer customer = new Customer
-                {
-                    UserId = collection["UserId"],
-                    FirstName = collection["FirstName"],
-                    LastName = collection["LastName"],
-                    Email = collection["Email"],
-                    TelephoneHome = collection["TelephoneHome"],
-                    TelephoneMobile = collection["TelephoneMobile"],
-                    TelephoneWork = collection["TelephoneWork"],
-                    AddressLine1 = collection["AddressLine1"],
-                    AddressLine2 = collection["AddressLine2"],
-                    AddressLine3 = collection["AddressLine3"],
-                    AddressLine4 = collection["AddressLine4"],
-                    AddressPostcode = collection["AddressPostcode"],
-                    Dealer = collection["Dealer"],
-                    RequestCallback = collection["RequestCallback"].Contains("true"),
-                    EmailResults = collection["EmailResults"].Contains("true")
-                };
-
-                string modelCode = "XN12";
-                Combobulator.DAL.Car dbCar = dbContext.GetCar(modelCode).FirstOrDefault();
-                Car car = new Car
-                {
-                    Id = dbCar.Id,
-                    Model = dbCar.Model,
-                    ModelCode = dbCar.ModelCode,
-                    Colour = dbCar.Colour,
-                    Engine = dbCar.Engine,
-                    DisplayName = dbCar.DisplayName,
-                    Type = dbCar.Type,
-                    CapacityScale = dbCar.CapacityScale,
-                    LuggageScale = dbCar.LuggageScale,
-                    Options = dbCar.Options,
-                    PriceScale = dbCar.PriceScale,
-                    Cost = dbCar.Cost,
-                    PerformanceScale = dbCar.PerformanceScale,
-                    MPH = dbCar.MPH,
-                    EconomyScale = dbCar.EconomyScale,
-                    MPG = dbCar.MPG,
-                    UsageScale = dbCar.UsageScale,
-                    Alt1 = dbCar.Alt1,
-                    Alt2 = dbCar.Alt2,
-                    Alt3 = dbCar.Alt3,
-                    TermsConditions = dbCar.TermsConditions
-                };
-
-                if (ViewData.ModelState.IsValid)
-                {
-                    if (customer.EmailResults == true)
-                    {
-                        Email.Instance.EmailMeResults(customer, car);
-                    }
-
-                    Utils.Instance.SendNewCustomerData(customer);
-                }
-                return View("~/Views/Results/_FormConfirmation.cshtml");
-            }
-            catch (Exception ex)
-            {
-                log.Error("NewCustomerSubmit", ex);
-                return View("~/Views/Results/_FormError.cshtml");
-            }
-        }
-
-        [HttpPost]
         public ActionResult ExistingCustomerSubmit(FormCollection collection)
         {
             try
@@ -130,22 +61,16 @@ namespace Combobulator.Controllers
 
                 if (ViewData.ModelState.IsValid)
                 {
-                    if (customer.EmailResults == true)
-                    {
-                        Email.Instance.EmailMeResults(customer, car);
-                    }
-
                     if (!string.IsNullOrEmpty(customer.UserId))
                     {
                         // Send to API
                         Func<Customer, bool> func = new Func<Customer, bool>(Utils.Instance.SendExistingCustomerDataApi);
-                        bool success = Utils.Instance.DoFuncWithRetry(func, customer, TimeSpan.FromSeconds(2));
-
+                        bool sent = Utils.Instance.DoFuncWithRetry(func, customer, TimeSpan.FromSeconds(2));
                         //Action action = () => Utils.Instance.SendExistingCustomerData(customer);
                         //Utils.Instance.DoWithRetry(action, TimeSpan.FromSeconds(2));
 
                         // If send api fails then send to fallback email
-                        if (!success)
+                        if (!sent)
                         {
                             Email.Instance.EmailCustomerDetails(customer);
                         }
@@ -154,6 +79,19 @@ namespace Combobulator.Controllers
                     {
                         // Email customer details
                         Email.Instance.EmailCustomerDetails(customer);
+                    }
+
+                    // Email results to customer
+                    if (customer.EmailResults == true)
+                    {
+                        try
+                        {
+                            Email.Instance.EmailMeResults(customer, car);
+                        }
+                        catch (Exception ex)
+                        {
+                            log.Error("EmailMeResults", ex);
+                        }
                     }
                 }
                 return View("~/Views/Results/_FormConfirmation.cshtml");
@@ -164,5 +102,74 @@ namespace Combobulator.Controllers
                 return View("~/Views/Results/_FormError.cshtml");
             }
         }
+
+        //[HttpPost]
+        //public ActionResult NewCustomerSubmit(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        Customer customer = new Customer
+        //        {
+        //            UserId = collection["UserId"],
+        //            FirstName = collection["FirstName"],
+        //            LastName = collection["LastName"],
+        //            Email = collection["Email"],
+        //            TelephoneHome = collection["TelephoneHome"],
+        //            TelephoneMobile = collection["TelephoneMobile"],
+        //            TelephoneWork = collection["TelephoneWork"],
+        //            AddressLine1 = collection["AddressLine1"],
+        //            AddressLine2 = collection["AddressLine2"],
+        //            AddressLine3 = collection["AddressLine3"],
+        //            AddressLine4 = collection["AddressLine4"],
+        //            AddressPostcode = collection["AddressPostcode"],
+        //            Dealer = collection["Dealer"],
+        //            RequestCallback = collection["RequestCallback"].Contains("true"),
+        //            EmailResults = collection["EmailResults"].Contains("true")
+        //        };
+
+        //        string modelCode = "XN12";
+        //        Combobulator.DAL.Car dbCar = dbContext.GetCar(modelCode).FirstOrDefault();
+        //        Car car = new Car
+        //        {
+        //            Id = dbCar.Id,
+        //            Model = dbCar.Model,
+        //            ModelCode = dbCar.ModelCode,
+        //            Colour = dbCar.Colour,
+        //            Engine = dbCar.Engine,
+        //            DisplayName = dbCar.DisplayName,
+        //            Type = dbCar.Type,
+        //            CapacityScale = dbCar.CapacityScale,
+        //            LuggageScale = dbCar.LuggageScale,
+        //            Options = dbCar.Options,
+        //            PriceScale = dbCar.PriceScale,
+        //            Cost = dbCar.Cost,
+        //            PerformanceScale = dbCar.PerformanceScale,
+        //            MPH = dbCar.MPH,
+        //            EconomyScale = dbCar.EconomyScale,
+        //            MPG = dbCar.MPG,
+        //            UsageScale = dbCar.UsageScale,
+        //            Alt1 = dbCar.Alt1,
+        //            Alt2 = dbCar.Alt2,
+        //            Alt3 = dbCar.Alt3,
+        //            TermsConditions = dbCar.TermsConditions
+        //        };
+
+        //        if (ViewData.ModelState.IsValid)
+        //        {
+        //            if (customer.EmailResults == true)
+        //            {
+        //                Email.Instance.EmailMeResults(customer, car);
+        //            }
+
+        //            Utils.Instance.SendNewCustomerData(customer);
+        //        }
+        //        return View("~/Views/Results/_FormConfirmation.cshtml");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        log.Error("NewCustomerSubmit", ex);
+        //        return View("~/Views/Results/_FormError.cshtml");
+        //    }
+        //}
     }
 }
