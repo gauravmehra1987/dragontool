82752ec65558a8675e609ec54bd71ae6e929495a
Keith Vong <keith.vong@live.co.uk>
2015-02-03 11:43:40 +0000

    assets preload controller

---
+++ dotnet/Combobulator/Controllers/AssetsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web;
+
+namespace Combobulator.Controllers
+{
+    public class AssetsController : ApiController
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        // GET api/assets/svg
+        // GET api/assets/png
+        [System.Web.Mvc.HttpGet]
+        public HttpResponseMessage Get(string id)
+        {
+            var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            IEnumerable<string> files = null;
+
+            try
+            {
+                if (id == "svg")
+                {
+                    files = Directory.EnumerateFiles(HttpContext.Current.Server.MapPath("~/assets/sprites/svg/"), "*.svg", SearchOption.AllDirectories).Select(Path.GetFileName).ToList();
+                    files = files.Select(x => x = ("assets/sprites/svg/" + x));
+                }
+
+                if (id == "png")
+                {
+                    files = Directory.EnumerateFiles(HttpContext.Current.Server.MapPath("~/assets/sprites/"), "*.png", SearchOption.AllDirectories).Select(Path.GetFileName).ToList();
+                    files = files.Select(x => x = ("assets/sprites/" + x));
+                }
+
+                StringContent sc = new StringContent(serializer.Serialize(files).ToString());
+                sc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response.Content = sc;
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                log.Error("", ex);
+
+                StringContent sc = new StringContent(serializer.Serialize(files).ToString());
+                sc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response.Content = sc;
+                return response;
+            }
+        }
+    }
+}
