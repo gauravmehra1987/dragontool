85e577d38b9c28a2c0bbcbadf97137d885d30d4e
Keith Vong <keith.vong@live.co.uk>
2015-02-03 09:59:47 +0000

    class files

---
+++ dotnet/Combobulator/Models/ErrorResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Combobulator.Models
+{
+    public class ErrorResponse
+    {
+        public string Error { get; set; }
+    }
+}
