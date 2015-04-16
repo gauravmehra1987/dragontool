d35ed7d04a84a7b95b0f1519d0476c226e698cb9
Jaime De Federico <jaime.defederico@iris-worldwide.com>
2015-04-16 17:47:58 +0100

    Simplify regex for phone numbers

--- assets/js/src/validation.rules.js
+++ assets/js/src/validation.rules.js
@@ -83,7 +83,7 @@ $.validator.addMethod(
 	'regexHomePhone',
 	function(value, element, regexp) {
 		var check = false;
-		var re = new RegExp('^(((\+44\s?|0044\s?)?|(\(?0))((2[03489]\)?\s?\d{4}\s?\d{4})|(1[23456789]1\)?\s?\d{3}\s?\d{4})|(1[23456789][234578][0234679]\)?\s?\d{6})|(1[2579][0245][0467]\)?\s?\d{5})|(11[345678]\)?\s?\d{3}\s?\d{4})|(1[35679][234689]\s?[46789][234567]\)?\s?\d{4,5})|([389]\d{2}\s?\d{3}\s?\d{4})|([57][0-9]\s?\d{4}\s?\d{4})|(500\s?\d{6})|(7[456789]\d{2}\s?\d{6})))$');
+		var re = new RegExp('^[0-9]| |\+)+$');
 		return this.optional(element) || re.test(value);
 	},
 	'Please enter a valid phone number.'
