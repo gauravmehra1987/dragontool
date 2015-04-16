2a8ae990fa4ae7fe5ad080eedd1eb1df3ee7dc8d
Jaime De Federico <jaime.defederico@iris-worldwide.com>
2015-04-16 11:44:51 +0100

    Amend postcode regex and add home phone number regex

--- assets/js/src/validation.rules.js
+++ assets/js/src/validation.rules.js
@@ -9,13 +9,18 @@ $.validator.setDefaults( {
 		'address-1':		{ required: true },
 		'address-2':		{ required: true },
 		'address-4':		{ required: true },
-		'tel-home':			{ required: true },
+		'tel-home':			{
+
+			required: true,
+			regexHomePhone: true
+
+		},
 		'dealer':			{ required: true },
 		'title':			{ required: true },
 		'postcode-search':	{
 
 			required: true,
-			regex: true
+			regexPostcode: true
 
 		},
 		'email':			{
@@ -52,11 +57,21 @@ $.validator.setDefaults( {
 } );
 
 $.validator.addMethod(
-	'regex',
+	'regexPostcode',
 	function(value, element, regexp) {
 		var check = false;
-		var re = new RegExp('^([a-zA-Z]){1}([0-9][0-9]|[0-9]|[a-zA-Z][0-9][a-zA-Z]|[a-zA-Z][0-9][0-9]|[a-zA-Z][0-9]){1}([        ])([0-9][a-zA-z][a-zA-z]){1}$');
+		var re = new RegExp('^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$', 'i');
 		return this.optional(element) || re.test(value);
 	},
 	'Please enter a valid UK postcode.'
+);
+
+$.validator.addMethod(
+	'regexHomePhone',
+	function(value, element, regexp) {
+		var check = false;
+		var re = new RegExp('^(((\+44\s?|0044\s?)?|(\(?0))((2[03489]\)?\s?\d{4}\s?\d{4})|(1[23456789]1\)?\s?\d{3}\s?\d{4})|(1[23456789][234578][0234679]\)?\s?\d{6})|(1[2579][0245][0467]\)?\s?\d{5})|(11[345678]\)?\s?\d{3}\s?\d{4})|(1[35679][234689]\s?[46789][234567]\)?\s?\d{4,5})|([389]\d{2}\s?\d{3}\s?\d{4})|([57][0-9]\s?\d{4}\s?\d{4})|(500\s?\d{6})|(7[456789]\d{2}\s?\d{6})))$');
+		return this.optional(element) || re.test(value);
+	},
+	'Please enter a valid phone number.'
 );
