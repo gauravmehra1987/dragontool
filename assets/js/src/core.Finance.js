861eff474d5b842b49938dec0ab8d8dca95c2db4
Matine Chabrier <matine.chabrier@iris-worldwide.com>
2015-05-12 11:23:33 +0100

    implemented if statement on finance table to hide rows if retailer deposit is 0

--- assets/js/src/core.Finance.js
+++ assets/js/src/core.Finance.js
@@ -20,11 +20,33 @@ function Finance() {
 				//
 				// Replace the contents of the div, with the template contents...
 				$( '#tpl-finance' ).replaceWith( ui.renderTpl( tpl, carFinance ) );
+
+				// When payment plan table exsists, run the hideRows function
+				runWhenElementExsists( $('#payment-plan'), hideRows );
 				//
+				// Check if contibution is 0 and if so, hide it and hide the customer deposit
+				function hideRows() {
+					//
+					if ( parseInt(carFinance.contribution) === 0 ) {
+						//
+						$('.retailer-deposit-contribution--row').addClass('hide');
+						$('.customer-deposit--row').addClass('hide');
+					}
+				}
+
 			} );
 		}
 	}
 
+
+	/**
+	 * TERMS DROPDOWN
+	*/
+	this.termsDropdown = function() {
+		//
+
+		}
+	}
 
 
 	/**
